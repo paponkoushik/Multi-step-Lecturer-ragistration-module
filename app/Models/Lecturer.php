8b055a708c0eb64e0a11081f29621994ffafe6d9
paponkoushik <paponkoushik@gmail.com>
2021-04-22 11:31:51 +0600

    model and migration file added for lecturer

---
+++ app/Models/Lecturer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lecturer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone_number', 'nid_number', 'avatar', 'nid_img'];
+}
---
+++ database/migrations/2021_04_22_051344_create_lecturers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('nid_number');
+            $table->string('avatar');
+            $table->string('nid_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturers');
+    }
+}
